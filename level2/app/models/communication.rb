ef0396b60fdd7be09d4b68067fa10cb73f0e47a0
Aymeric Vinot <aymeric.vinot@gmail.com>
2019-04-03 11:24:15 +0200

    improving perf for index, logic for create, adding api view

--- level2/app/models/communication.rb
+++ level2/app/models/communication.rb
@@ -1,12 +1,13 @@
 class Communication < ApplicationRecord
   belongs_to :practitioner
 
-  def as_json(options = nil)
-    {
-      first_name: practitioner.first_name,
-      last_name: practitioner.last_name,
-      sent_at: sent_at
-    }
-  end
+  ## Why do we need that ?
+  # def as_json(options = nil)
+  #   {
+  #     first_name: practitioner.first_name,
+  #     last_name: practitioner.last_name,
+  #     sent_at: sent_at
+  #   }
+  # end
 
 end
