a4efc3dd77955ad78c77af3a5d69e076c62ac1da
Aymeric Vinot <aymeric.vinot@gmail.com>
2019-04-09 12:19:44 +0200

    removed practitioner model as it's useless in the scope of the exercice

--- level1/models/communication.rb
+++ level1/models/communication.rb
@@ -1,24 +1,32 @@
 require 'date'
 
 class Communication
-  attr_reader :practitioner
 
-  def initialize(id, pages_number, color, sent_at, practitioner)
+  COST_OF_FIRST_PAGE = 10
+  COST_OF_NEXT_PAGE = 7
+  COST_OF_COLOR = 18
+  COST_OF_EXPRESS_DELIVERY = 60
+
+  def initialize(id, pages_number, color, sent_at, express_delivery)
     @id = id
     @pages_number = pages_number
     @color = color
     @sent_at = sent_at
-    @practitioner = practitioner
+    @express_delivery = express_delivery
   end
 
   def has_color?
     @color
   end
 
+  def express_delivery?
+    @express_delivery
+  end
+
   def cost_in_cents
-    cost_in_cents = 10 + 7 * (@pages_number - 1)
-    cost_in_cents += 18 if has_color?
-    cost_in_cents += 60 if @practitioner.has_express_delivery?
+    cost_in_cents = COST_OF_FIRST_PAGE + COST_OF_NEXT_PAGE * (@pages_number - 1)
+    cost_in_cents += COST_OF_COLOR if has_color?
+    cost_in_cents += COST_OF_EXPRESS_DELIVERY if express_delivery?
 
     cost_in_cents
   end
